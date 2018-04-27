b42065e55b42e20e4a45d2a17235741e842c30ce
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2018-04-27 09:54:18 -0700

    Clean up generated annotation.

--- src/main/java/FreeBuilderHandler.java
+++ src/main/java/FreeBuilderHandler.java
@@ -16,6 +16,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class FreeBuilderHandler implements CodeInsightActionHandler {
@@ -27,23 +28,27 @@ public class FreeBuilderHandler implements CodeInsightActionHandler {
       Arrays.stream(psiJavaFile.getClasses())
           .filter(psiClass -> psiClass.getName().equals(fileNameWithoutExtension))
           .forEach(psiClass -> {
-            PsiAnnotation freeBuilderAnnotation = annotate(project, psiClass, FreeBuilder.class, Collections.emptyMap(), null);
+            annotate(project, psiClass, FreeBuilder.class, Collections.emptyMap(), null);
             addBuilderClass(project, psiClass);
-            addJacksonAnnotation(project, psiClass, freeBuilderAnnotation);
+            addJacksonAnnotation(project, psiClass);
             rebuild(project);
             UndoUtil.markPsiFileForUndo(file);
           });
    }
   }
 
-  private void addJacksonAnnotation(Project project, PsiClass psiClass, PsiAnnotation freeBuilderAnnotation) {
+  private void addJacksonAnnotation(Project project, PsiClass psiClass) {
     Module moduleForPsiElement = ModuleUtil.findModuleForPsiElement(psiClass);
     PsiClass jacksonAnnotationClass = JavaPsiFacade.getInstance(project)
         .findClass(JsonDeserialize.class.getCanonicalName(),
             GlobalSearchScope.moduleRuntimeScope(moduleForPsiElement, false));
     if (jacksonAnnotationClass != null) {
+      Optional<PsiAnnotation> annotation = Arrays.stream(psiClass.getAnnotations())
+          .filter(psiAnnotation -> psiAnnotation.getQualifiedName().equals(FreeBuilder.class.getCanonicalName()))
+          .findFirst();
       annotate(project, psiClass, JsonDeserialize.class,
-          Collections.singletonMap("builder", String.format("%s.Builder.class", psiClass.getName())), freeBuilderAnnotation);
+          Collections.singletonMap("builder", String.format("%s.Builder.class", psiClass.getName())),
+          annotation.orElse(null));
     }
   }
 
@@ -81,25 +86,26 @@ public class FreeBuilderHandler implements CodeInsightActionHandler {
     }
   }
 
-  private PsiAnnotation annotate(Project project, PsiClass psiClass,
+  private void annotate(Project project, PsiClass psiClass,
                                  Class annotationClass, Map<String, String> attributes, PsiAnnotation anchor) {
     boolean annotationNotPresent = Arrays.stream(psiClass.getAnnotations())
         .noneMatch(annotation -> annotation.getQualifiedName().equals(annotationClass.getCanonicalName()));
     if (annotationNotPresent) {
       PsiModifierList modifierList = psiClass.getModifierList();
-      StringBuilder stringBuilder = new StringBuilder(String.format("@%s(", annotationClass.getName()));
-      stringBuilder.append(attributes.entrySet().stream()
-          .map(entry -> String.format("%s = %s", entry.getKey(), entry.getValue()))
-          .collect(Collectors.joining(", ")));
-      stringBuilder.append(")");
-      PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project)
+      StringBuilder stringBuilder = new StringBuilder(String.format("@%s", annotationClass.getName()));
+      if(!attributes.isEmpty()) {
+        stringBuilder.append("(");
+        stringBuilder.append(attributes.entrySet().stream()
+            .map(entry -> String.format("%s = %s", entry.getKey(), entry.getValue()))
+            .collect(Collectors.joining(", ")));
+        stringBuilder.append(")");
+      }
+     PsiElementFactory elementFactory = JavaPsiFacade.getInstance(project)
           .getElementFactory();
       PsiAnnotation psiAnnotation = elementFactory
           .createAnnotationFromText(stringBuilder.toString(), psiClass);
       modifierList.addAfter(psiAnnotation, anchor);
       JavaCodeStyleManager.getInstance(project).shortenClassReferences(psiClass);
-      return psiAnnotation;
     }
-    return null;
   }
 }
